9424b24635b8c119d0bbf296b6071786b2ed049c
Atharva pimparkar <atharvapimparkar1416@gmail.com>
2024-10-24 15:42:04 +0530

    First Commit

---
+++ src/main/java/com/CollgeManagement/Project/CollegeManagemenApplication.java
@@ -0,0 +1,13 @@
+package com.CollgeManagement.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagemenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagemenApplication.class, args);
+		System.out.println("Run...");
+	}
+}  
